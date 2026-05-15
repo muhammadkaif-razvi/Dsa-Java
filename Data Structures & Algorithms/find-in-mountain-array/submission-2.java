89e3b8cf9e7fd87e7e939d5e71d137216de5a58f
Muhammad Kaif Razvi <muhammadkaifrazwi@gmail.com>
2026-05-15 21:10:36 +0530

    Add: find-in-mountain-array - submission-2

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-2.java
@@ -0,0 +1,57 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+    int length = mountainArr.length();
+    int l = 1,r = length - 2,peak = 0;
+    while(l <= r){
+        int m = (l+r)/2;
+        int left = mountainArr.get(m-1);
+        int mid = mountainArr.get(m);
+        int right = mountainArr.get(m+1);
+        if(left < mid && mid < right){
+            l = m + 1;
+        }else if(left > mid && mid > right){
+            r = m - 1;
+        }else {
+            peak = m;
+            break;
+        }
+    }   
+    // search left portion
+    l = 0;
+    r = peak - 1;
+    while (l <= r){
+        int m = (l+r)/2;
+        int val = mountainArr.get(m);
+        if(val < target){
+            l = m + 1;
+        }else if( val > target){
+            r = m -1;
+        }else {
+            return m;
+        }
+    }
+    l = peak;
+    r = length -1;
+    while(l <= r){
+        int m = (l+r)/2;
+        int val = mountainArr.get(m);
+        if(val > target){
+            l = m + 1;
+        }else if(val < target){
+            r = m -1;
+        }else {
+            return m;
+        }
+    }
+    return -1;
+    }
+}
