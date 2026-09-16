64ec288f309e20ec74413e8393bc2155590a8c45
Muhammad Kaif Razvi <191553328+muhammadkaif-razvi@users.noreply.github.com>
2026-09-17 00:26:38 +0530

    Add: find-k-closest-elements - submission-3

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int l = 0,r = arr.length - k;
+        while(l<r){
+            int m = (r+l)/2;
+            if(x - arr[m] <= arr[m+k] - x ){
+                r = m;
+            }else{
+                l = m+1;
+            }
+            
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int i = l;i<l+k;i++){
+            res.add(arr[i]);
+        }
+        return res;
+    }
+}
